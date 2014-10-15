ecedfb29081923c74966d26e6cfeed36147b9ab1
johnqing <csssnow@gmail.com>
2014-10-15 18:58:06 +0800

    add sc

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 var rank = require('./lib/ranking');
 var setting = require('./setting');
 
-var ranking = function(n){
+var ranking = function(n, sc){
 	setting.number = n || 5;
+	setting.sc = sc == 'y' ? 'yi7' : 'dwjz';
 	rank(setting);
 };
-
 module.exports = ranking;
--- setting.js
+++ setting.js
@@ -1,3 +1,3 @@
 module.exports = {
-	url: 'http://fund.eastmoney.com/data/rankhandler.aspx?op=ph&dt=hb&ft=&rs=&gs=0&sc=dwjz&st=desc&pi=1&mg=&dx=1&v=0.7395475923549384'
+	url: 'http://fund.eastmoney.com/data/rankhandler.aspx?op=ph&dt=hb&ft=&rs=&gs=0&st=desc&pi=1&mg=&dx=1&v=0.7395475923549384'
 };
